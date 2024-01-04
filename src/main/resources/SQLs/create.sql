da358aedf582f26b5ed954072604ca926ef7312d
UmeshSulakude <Sulakudeumesh@gmail.com>
2024-01-04 17:17:18 +0530

    sql

--- src/main/resources/SQLs/create.sql
+++ src/main/resources/SQLs/create.sql
@@ -6,3 +6,17 @@ CREATE TABLE `student` (
                            PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+CREATE TABLE `department` (
+    `id` bigint NOT NULL AUTO_INCREMENT,
+    `dept_name` varchar(255) DEFAULT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+
+ALTER TABLE student
+ADD COLUMN department_id bigint,
+ADD CONSTRAINT fk_department
+    FOREIGN KEY (department_id)
+    REFERENCES department(id);
+
